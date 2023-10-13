0497a9f5508ab9fe279534361a6f286feda8f553
leonardostefanini <136688776+leonardostefanini@users.noreply.github.com>
2023-10-13 10:15:55 +0200

    separazione login registrazione

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,61 +1,61 @@
-btnregistrami.addEventListener("click", function(){
-    const URL1 = "http://localhost:9006/api/utente";
+// btnregistrami.addEventListener("click", function(){
+//     const URL1 = "http://localhost:9006/api/utente";
 
 
-    let inputUser = document.querySelector("#user").value;
-    let inputPassword = document.querySelector("#password").value;
-    let inputFirma = document.querySelector("#firma").value;
+//     let inputUser = document.querySelector("#user").value;
+//     let inputPassword = document.querySelector("#password").value;
+//     let inputFirma = document.querySelector("#firma").value;
 
-    let inputNome = document.querySelector("#nome").value;
-    let inputCognome = document.querySelector("#cognome").value;
-    let inputNascita = document.querySelector("#nascita").value;
-    let inputEmail = document.querySelector("#email").value;
+//     let inputNome = document.querySelector("#nome").value;
+//     let inputCognome = document.querySelector("#cognome").value;
+//     let inputNascita = document.querySelector("#nascita").value;
+//     let inputEmail = document.querySelector("#email").value;
    
 
-    let nuovoUser = {
-        user: inputUser,
-        password: inputPassword,
-        firma: inputFirma,
-        tipo: "U",
-        nome: inputNome,
-        cognome: inputCognome,
-        nascita: inputNascita,
-        email: inputEmail,
+//     let nuovoUser = {
+//         user: inputUser,
+//         password: inputPassword,
+//         firma: inputFirma,
+//         tipo: "U",
+//         nome: inputNome,
+//         cognome: inputCognome,
+//         nascita: inputNascita,
+//         email: inputEmail,
        
-    }
+//     }
 
-    fetch(URL1, {
-        method: "POST",
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(nuovoUser)
-    })
-    .then(
-        data =>{ return data.json() }
-    )
-    .then(response=>{
-        location.href="index.html";
+//     fetch(URL1, {
+//         method: "POST",
+//         headers: {
+//             'Content-type': 'application/json'
+//         },
+//         body: JSON.stringify(nuovoUser)
+//     })
+//     .then(
+//         data =>{ return data.json() }
+//     )
+//     .then(response=>{
+//         location.href="index.html";
         
                         
-                    })
-})
+//                     })
+// })
 
 let login = document.querySelector("#login");
-let registrami = document.querySelector("#registrami");
+// let registrami = document.querySelector("#registrami");
 
-registrami.addEventListener("click",function(){
+// registrami.addEventListener("click",function(){
 
-    let sfondo = document.querySelector("#sfondo");
+//     let sfondo = document.querySelector("#sfondo");
 
-    if(sfondo.classList.contains("d-none")){
+//     if(sfondo.classList.contains("d-none")){
 
-        sfondo.classList.remove("d-none")
-    }else{
-        sfondo.classList.add("d-none")
-    }
+//         sfondo.classList.remove("d-none")
+//     }else{
+//         sfondo.classList.add("d-none")
+//     }
 
-})
+// })
 
 let btnLogin = document.querySelector("#btnLogin")
 btnLogin.addEventListener("click",function(){
---
+++ frontend/js/registrazione.js
@@ -0,0 +1,56 @@
+btnregistrami.addEventListener("click", function(){
+    const URL1 = "http://localhost:9006/api/utente";
+
+
+    let inputUser = document.querySelector("#user").value;
+    let inputPassword = document.querySelector("#password").value;
+    let inputFirma = document.querySelector("#firma").value;
+
+    let inputNome = document.querySelector("#nome").value;
+    let inputCognome = document.querySelector("#cognome").value;
+    let inputNascita = document.querySelector("#nascita").value;
+    let inputEmail = document.querySelector("#email").value;
+   
+
+    let nuovoUser = {
+        user: inputUser,
+        password: inputPassword,
+        firma: inputFirma,
+        tipo: "U",
+        nome: inputNome,
+        cognome: inputCognome,
+        nascita: inputNascita,
+        email: inputEmail,
+       
+    }
+
+    fetch(URL1, {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(nuovoUser)
+    })
+    .then(
+        data =>{ return data.json() }
+    )
+    .then(response=>{
+        location.href="index.html";
+        
+                        
+                    })
+})
+let registrami = document.querySelector("#registrami");
+
+registrami.addEventListener("click",function(){
+
+    let sfondo = document.querySelector("#sfondo");
+
+    if(sfondo.classList.contains("d-none")){
+
+        sfondo.classList.remove("d-none")
+    }else{
+        sfondo.classList.add("d-none")
+    }
+
+})
