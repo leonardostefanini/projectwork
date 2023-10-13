91fcbb666e8ec8775e52e7e5d202957ef45e1682
Gianmarco98-ops <gianmarcomarrandino@gmail.com>
2023-10-13 16:08:27 +0200

    boh

--- backend/src/main/java/it/project/integration/RestCtrl.java
+++ backend/src/main/java/it/project/integration/RestCtrl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import it.project.services.VeicoloService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class RestCtrl {
 	
 	@Autowired
---
+++ frontend/js/cardauto.js
@@ -0,0 +1,42 @@
+const URL = "http://localhost:9020/api/veicolo";
+let disp = document.querySelector("#VeicDisp");
+let indisp = document.querySelector("#VeicNonDisp");
+
+fetch(URL)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    data.forEach(veicolo => {
+
+      const disponibilita = veicolo.disponibilità;
+      console.log("Disponibilità:", disponibilita);
+
+      if (disponibilita === true) {
+        disp.innerHTML += creaCard(veicolo);
+      } else {
+        indisp.innerHTML += creaCard(veicolo);
+      }
+    });
+  })
+  .catch(error => console.error("Errore durante la richiesta:", error));
+
+function creaCard(veicolo) {
+    return `<div class="card border-success">
+    <div class="card-body d-flex align-items-center">
+      <div class="w-50">
+        <img src="${veicolo.immagine}" class="img-fluid w-75 h-75">
+      </div>
+      <div class="w-50 ms-4">
+        <h5 class="card-title"><strong>Tipologia:</strong> ${veicolo.tipologia}</h5>
+        <p class="card-text"><strong>Alimentazione:</strong> ${veicolo.alimentazione}</p>
+        <p class="card-text"><strong>Disponibilità:</strong> ${veicolo.disponibilità}</p>
+        <button class="btn btn-primary" onclick="reindirizzo('${veicolo.id}')">Vai all'auto</button>
+      </div>
+    </div>
+  </div>`;
+}
+
+function reindirizzo(id) 
+{
+  window.location.href = `http://localhost:9020/api/veicolo/${id}`;
+}
