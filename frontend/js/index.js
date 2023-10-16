70ca77f04804e025eca9733c1621a3d3f1e76957
leonardostefanini <136688776+leonardostefanini@users.noreply.github.com>
2023-10-16 12:14:38 +0200

    slider

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,4 +1,4 @@
-/* var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
@@ -11,7 +11,7 @@
     prevEl: ".swiper-button-prev",
   },
   autoplay: {
-    delay: 1500, // interval in milliseconds between slides
+    delay: 3000, // interval in milliseconds between slides
     disableOnInteraction: false,
   },
 });
@@ -33,12 +33,14 @@ function auto() {
           let immagine=td.immagine;
           let tipologia=td.tipologia;
           let descrizione=td.descrizione;
+          let alimentazione=td.alimentazione;
 
-          swiper.appendSlide(`<div class="swiper-slide bg-danger">
+          swiper.appendSlide(`<div class="swiper-slide ">
           <img class="card-img-top" src="${immagine}" alt="Title">
           <div class="card-body">
-              <h4 class="card-title">${tipologia}</h4>
-              <p class="card-text">${descrizione}</p>
+              <h3 class="card-title"><strong>Tipologia:</strong> ${tipologia}</h3>
+              <p class="card-text"><strong>Alimentazione:</strong> ${alimentazione}</p>
+              <p class="card-text"><strong>Caratteristiche:</strong> ${descrizione}</p>
           </div>
           
           </div>`);
@@ -49,17 +51,16 @@ function auto() {
   
 }
 
-//   function slide(immagine,tipologia,descrizione) {
+  //  function slide(immagine,tipologia,descrizione) {
 
-//       swiper.appendSlide(`<div class="swiper-slide bg-danger">
-//       <img class="card-img-top" src="${immagine}" alt="Title">
-//       <div class="card-body">
-//           <h4 class="card-title">${tipologia}</h4>
-//           <p class="card-text">${descrizione}</p>
-//       </div>
+  //      swiper.appendSlide(`<div class="swiper-slide bg-danger">
+  //     <div class="card-body">
+  //          <h4 class="card-title">${tipologia}</h4>
+  //          <p class="card-text">${descrizione}</p>
+  //      </div>
     
-//       </div>`);
-//     }
+  //     </div>`);
+  //    }
 
 
-auto(); */
+auto();
