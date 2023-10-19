e358e72d64c6d30dd3626bb6d59109f0d2324519
leonardostefanini <136688776+leonardostefanini@users.noreply.github.com>
2023-10-19 11:59:22 +0200

    a

--- frontend/js/cardauto.js
+++ frontend/js/cardauto.js
@@ -57,3 +57,67 @@ function reindirizzo(id)
   localStorage.setItem('autoId', id);
   window.location.href = "auto.html";
 }
+//////////////////////////////////////////////////// aggiunta leooooo///////////////////////////////////////////////////////////
+ 
+
+
+//  fetch(URL)
+//    .then(response => response.json())
+//    .then(data => {
+//      data.forEach(veicolo => {
+//        const disponibilita = veicolo.disponibilita;
+//        if (disponibilita === true) {
+//          disp.innerHTML += creaCardDisp(veicolo);
+//        } else {
+//          indisp.innerHTML += creaCardIndisp(veicolo);
+//        }
+//      });
+//      // Iniziale visualizzazione dei veicoli e dei pulsanti di paginazione
+//      displayVehicles();
+//      generatePaginationButtons();
+//    })
+//    .catch(error => console.error("Errore durante la richiesta:", error));
+
+//  // Numero di veicoli da visualizzare per pagina
+//  const vehiclesPerPage = 10;
+//  let currentPage = 1;
+
+//  // Funzione per mostrare i veicoli nella pagina corrente
+//  function displayVehicles() {
+//    const startIndex = (currentPage - 1) * vehiclesPerPage;
+//    const endIndex = startIndex + vehiclesPerPage;
+//    const vehiclesToShow = Array.from(document.querySelectorAll(".card")).slice(startIndex, endIndex);
+
+//    // Nascondi tutti i veicoli
+//    Array.from(document.querySelectorAll(".card")).forEach(vehicle => {
+//      vehicle.style.display = "none";
+//    });
+
+//    // Mostra solo i veicoli nella pagina corrente
+//    vehiclesToShow.forEach(vehicle => {
+//      vehicle.style.display = "block";
+//    });
+//  }
+
+//  // Funzione per generare i pulsanti di paginazione
+//  function generatePaginationButtons() {
+//   const totalPages = Math.ceil(document.querySelectorAll(".card").length / vehiclesPerPage);
+//   const paginationElement = document.getElementById("pagination");
+//   paginationElement.innerHTML = "";
+
+//   for (let i = 1; i <= totalPages; i++) {
+//     const pageLink = document.createElement("a");
+//     pageLink.className = "page-link";
+//     pageLink.textContent = i;
+//     pageLink.href = "#";
+//     pageLink.addEventListener("click", () => {
+//       currentPage = i;
+//       displayVehicles();
+//       // Aggiungi il codice per tornare al div con ID "veicoli"
+//       document.getElementById('veicoli').scrollIntoView({ behavior: 'smooth' });
+//     });
+
+//     paginationElement.appendChild(pageLink);
+//   }
+// }
+
